9912c9b3fb7ddce6c5d9ae251f310a3ab90fe767
David Adi Nugroho <davidadi216@gmail.com>
2019-11-02 12:10:02 +0700

    bundling extension

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const path = require('path');
+
+const config = {
+	target: 'node',
+	entry: './src/extension.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'extension.js',
+		libraryTarget: 'commonjs2',
+		// devtoolModuleFilenameTemplate: '../[resource-path]'
+	},
+	devtool: 'source-map',
+	externals: {
+		vscode: 'commonjs vscode'
+	},
+	resolve: {
+		extensions: ['.js']
+	},
+	module: {
+		rules: []
+	}
+};
+
+module.exports = config;
